d483015b90af91d9d15721c944a34d16418731eb
Niklas Knaack <n.knaack@anymotion.de>
2021-01-10 00:47:29 +0100

    some more optimizations to collision detection and a bit of code cleanup

--- js/vehicles/CollisionDetection.js
+++ js/vehicles/CollisionDetection.js
@@ -194,8 +194,8 @@ class CollisionDetection {
 
             vehicle.collisionDetected = false;
             vehicle.drawTempStuff = [];
-            vehicle.collisions = [];
-            //vehicle.speed = this._vehicles.getVehicleSpeed( vehicle, Vehicles.VEHICLE_SPEED );
+            // vehicle.collisions = [];
+            // vehicle.speed = this._vehicles.getVehicleSpeed( vehicle, Vehicles.VEHICLE_SPEED );
 
         }
 
@@ -386,30 +386,8 @@ class CollisionDetection {
 
                         //---
 
-                        // const radius = Vehicles.VEHICLE_RADIUS * 3;
-
                         const angle01 = Math.atan2( vehicle1.position.y - vehicle0.position.y, vehicle1.position.x - vehicle0.position.x ) + Settings.MATH_PI_050;
                         const angle10 = Math.atan2( vehicle0.position.y - vehicle1.position.y, vehicle0.position.x - vehicle1.position.x ) + Settings.MATH_PI_050;
-                        // // const angle01 = vehicle1.angle + Settings.MATH_PI_050;
-                        // // const angle10 = vehicle0.angle + Settings.MATH_PI_050;
-
-                        // const sin01 = Math.sin( angle01 );
-                        // const cos01 = Math.cos( angle01 );
-                        // const sin10 = Math.sin( angle10 );
-                        // const cos10 = Math.cos( angle10 );
-
-                        // const px01 = sin01 * radius + vehicle0.position.x;
-                        // const py01 = -cos01 * radius + vehicle0.position.y;
-                        // const px10 = sin10 * radius + vehicle1.position.x;
-                        // const py10 = -cos10 * radius + vehicle1.position.y;
-
-                        // // vehicle0.drawTempStuff = [];
-                        // // vehicle1.drawTempStuff = [];
-
-                        // vehicle0.drawTempStuff.push( { x0: vehicle0.position.x, y0: vehicle0.position.y, x1: px01, y1: py01, r: 0, g: 255, b: 0, a: 255 } );
-                        // vehicle1.drawTempStuff.push( { x0: vehicle1.position.x, y0: vehicle1.position.y, x1: px10, y1: py10, r: 255, g: 255, b: 255, a: 255 } );
-
-                        //vehicle1.drawTempStuff.push( { x0: vehicle1.position.x, y0: vehicle1.position.x, x1: px10, y1: py10, r: 255, g: 0, b: 0, a: 255 } );
 
                         //---
 
@@ -423,19 +401,16 @@ class CollisionDetection {
                         if ( angleFront0 - Settings.MATH_PI_025 < angle01 && angleFront0 + Settings.MATH_PI_025 > angle01 ) {
 
                             vehicle0CollisionDetected = true;
-                            //vehicle0.collisions.push( 0 );
 
                         }
 
                         if ( angleFront1 - Settings.MATH_PI_025 < angle10 && angleFront1 + Settings.MATH_PI_025 > angle10 ) {
 
                             vehicle1CollisionDetected = true;
-                            //vehicle1.collisions.push( 0 );
 
                         }
 
-                        //if ( vehicle0.speed > 0 && vehicle1.speed > 0 ) {
-                        //if ( vehicle0CollisionDetected === false || vehicle1CollisionDetected === false ) {
+                        if ( vehicle0CollisionDetected === false || vehicle1CollisionDetected === false ) {
 
                             //distance um zu prüfen ob sich die vehicles annähern oder entfernen.
                             const distance = Tools.getDistance( vehicle0.position, vehicle1.position );
@@ -443,57 +418,22 @@ class CollisionDetection {
                             vehicle0.distanceToVehicle = distance;
                             vehicle1.distanceToVehicle = distance;
 
-                        //}
-
-                        // console.log( Tools.isLeft( vehicle0.lastPosition, vehicle0.position, vehicle1.position ) );
-
-                        const radius = Vehicles.VEHICLE_RADIUS;
-
-                        // const angleBack0 = vehicle0.angle + Settings.MATH_PI_050;
-                        // const angleBack1 = vehicle1.angle + Settings.MATH_PI_050;
-
-                        // const sin01 = Math.sin( angleFront0 );
-                        // const cos01 = Math.cos( angleFront0 );
-                        // const sin10 = Math.sin( angleFront1 );
-                        // const cos10 = Math.cos( angleFront1 );
-
-                        // const px01 = sin01 * radius + vehicle0.position.x;
-                        // const py01 = -cos01 * radius + vehicle0.position.y;
-                        // const px10 = sin10 * radius + vehicle1.position.x;
-                        // const py10 = -cos10 * radius + vehicle1.position.y;
-
-                        // const p0 = { x: px01, y: py01 };
-                        // const p1 = { x: px10, y: py10 };
+                        }
 
                         if ( vehicle0CollisionDetected === false && vehicle1CollisionDetected === false ) {
-                        //if ( vehicle0CollisionDetected === false ) {
 
-                            //vehicle0.position, vehicle0.lastPosition = rechts vor links, vehicle0.lastPosition, vehicle0.position = links vor rechts
-                            // if ( Tools.isLeft( vehicle0.position, vehicle0.lastPosition, vehicle1.position ) === true && vehicle0.distanceToVehicle < vehicle0.lastDistanceToVehicle ) {
-                            //if ( Tools.isLeft( vehicle0.position, p0, vehicle1.position ) === true && vehicle0.distanceToVehicle < vehicle0.lastDistanceToVehicle ) {
                             if ( Tools.isLeft( vehicle0.lastPosition, vehicle0.position, vehicle1.position ) === true && vehicle0.distanceToVehicle < vehicle0.lastDistanceToVehicle ) {
 
-                                //vehicle0.collisionDetected = true;
                                 vehicle0CollisionDetected = true;
-                                //vehicle0.collisions.push( 1 );
 
                             }
 
-                        //}
-
-                        //if ( vehicle1CollisionDetected === false ) {
-
-                            // if ( Tools.isLeft( vehicle1.position, vehicle1.lastPosition, vehicle0.position ) === true && vehicle1.distanceToVehicle < vehicle1.lastDistanceToVehicle ) {
-                            //if ( Tools.isLeft( vehicle1.position, p1, vehicle0.position ) === true && vehicle1.distanceToVehicle < vehicle1.lastDistanceToVehicle ) {
                             if ( Tools.isLeft( vehicle1.lastPosition, vehicle1.position, vehicle0.position ) === true && vehicle1.distanceToVehicle < vehicle1.lastDistanceToVehicle ) {
 
-                                //vehicle1.collisionDetected = true;
                                 vehicle1CollisionDetected = true;
-                                //vehicle1.collisions.push( 1 );
 
                             }
 
-                        //}
                         }
 
                         vehicle0.lastDistanceToVehicle = vehicle0.distanceToVehicle;
@@ -504,151 +444,14 @@ class CollisionDetection {
                         if ( vehicle0CollisionDetected === true ) {
 
                             vehicle0.collisionDetected = true;
-                            vehicle0.collisions.push( vehicle1 );
 
                         }
 
                         if ( vehicle1CollisionDetected === true ) {
 
                             vehicle1.collisionDetected = true;
-                            vehicle1.collisions.push( vehicle0 );
 
                         }
-
-                        //---
-
-                        // const angleFront0 = vehicle0.angle + Settings.MATH_PI_050;
-                        // const angleFront1 = vehicle1.angle + Settings.MATH_PI_050;
-
-                        // if ( angleFront0 - Settings.MATH_PI_025 < angle01 && angleFront0 + Settings.MATH_PI_025 > angle01 ) {
-                        // //if ( angleFront0 + Settings.MATH_PI_025 < angle01 && angleFront0 + Settings.MATH_PI_075 > angle01 ) {
-
-                        //     vehicle0.collisionDetected = true;
-                        //     //vehicle0.speed = 0;
-
-                        // } else {
-
-                        //     //vehicle0.collisionDetected = false;
-
-                        // }
-
-                        // if ( angleFront1 - Settings.MATH_PI_025 < angle10 && angleFront1 + Settings.MATH_PI_025 > angle10 ) {
-                        // //if ( angleFront1 + Settings.MATH_PI_025 < angle10 && angleFront1 + Settings.MATH_PI_075 > angle10 ) {
-
-                        //     vehicle1.collisionDetected = true;
-                        //     //vehicle1.speed = 0;
-
-                        // } else {
-
-                        //     //vehicle1.collisionDetected = false;
-
-                        // }
-
-                        // const angleRight00 = angleFront0 + Settings.MATH_PI_025;
-                        // const angleRight01 = angleFront0 + Settings.MATH_PI_075;
-                        // const angleRight10 = angleFront1 + Settings.MATH_PI_025;
-                        // const angleRight11 = angleFront1 + Settings.MATH_PI_075;
-
-                        // const sin001 = Math.sin( angleRight00 );
-                        // const cos001 = Math.cos( angleRight00 );
-                        // const sin010 = Math.sin( angleRight01 );
-                        // const cos010 = Math.cos( angleRight01 );
-                        // const sin101 = Math.sin( angleRight10 );
-                        // const cos101 = Math.cos( angleRight10 );
-                        // const sin110 = Math.sin( angleRight11 );
-                        // const cos110 = Math.cos( angleRight11 );
-
-                        // const px001 = sin001 * radius + vehicle0.position.x;
-                        // const py001 = -cos001 * radius + vehicle0.position.y;
-                        // const px010 = sin010 * radius + vehicle0.position.x;
-                        // const py010 = -cos010 * radius + vehicle0.position.y;
-                        // const px101 = sin101 * radius + vehicle1.position.x;
-                        // const py101 = -cos101 * radius + vehicle1.position.y;
-                        // const px110 = sin110 * radius + vehicle1.position.x;
-                        // const py110 = -cos110 * radius + vehicle1.position.y;
-
-                        // vehicle0.drawTempStuff.push( { x0: vehicle0.position.x, y0: vehicle0.position.y, x1: px001, y1: py001, r: 25, g: 25, b: 255, a: 255 } );
-                        // vehicle0.drawTempStuff.push( { x0: vehicle0.position.x, y0: vehicle0.position.y, x1: px010, y1: py010, r: 25, g: 25, b: 255, a: 255 } );
-                        // vehicle1.drawTempStuff.push( { x0: vehicle1.position.x, y0: vehicle1.position.y, x1: px101, y1: py101, r: 25, g: 25, b: 255, a: 255 } );
-                        // vehicle1.drawTempStuff.push( { x0: vehicle1.position.x, y0: vehicle1.position.y, x1: px110, y1: py110, r: 25, g: 25, b: 255, a: 255 } );
-
-
-
-
-
-
-
-                        //console.clear();
-                        //console.log( vehicle0.angle, vehicle1.angle );
-
-                        // const normalizeAngle = ( angle ) => {
-
-                        //     return Math.atan2( Math.sin( angle ), Math.cos( angle ) );
-                            
-                        // }
-
-                        // const angleFront0 = vehicle0.angle + Settings.MATH_PI_050;
-                        // const angleFront1 = vehicle1.angle + Settings.MATH_PI_050;
-
-                        // const angle01 = Math.atan2( vehicle1.position.y - vehicle0.position.y, vehicle1.position.x - vehicle0.position.x );
-                        // const angle10 = Math.atan2( vehicle0.position.y - vehicle1.position.y, vehicle0.position.x - vehicle1.position.x );
-
-                        // //console.clear();
-                        // //console.log( angle01, angle10 );
-
-                        // // if ( angleFront0 - Settings.MATH_PI_025 < angleFront1 && angleFront0 + Settings.MATH_PI_025 > angleFront1 ) {
-                        // if ( angleFront0 - Settings.MATH_PI_025 < angle01 && angleFront0 + Settings.MATH_PI_025 > angle01 ) {
-
-                        //     vehicle0.collisionDetected = true;
-
-                        // }
-
-                        // // if ( angleFront1 - Settings.MATH_PI_025 < angleFront0 && angleFront1 + Settings.MATH_PI_025 > angleFront0 ) {
-                        // if ( angleFront1 - Settings.MATH_PI_025 < angle10 && angleFront1 + Settings.MATH_PI_025 > angle10 ) {
-
-                        //     vehicle1.collisionDetected = true;
-
-                        // }
-
-                        // https://jsfiddle.net/NiklasKnaack/dhzu1qwk/
-                        //funktioniert noch nicht:
-                        // const angleFront0 = vehicle0.angle + Settings.MATH_PI_050;
-                        // const angleFront1 = vehicle1.angle + Settings.MATH_PI_050;
-
-                        // if ( angleFront0 - Settings.MATH_PI_025 < angleFront1 && angleFront0 + Settings.MATH_PI_025 > angleFront1 ) {
-
-                        //     vehicle0.collisionDetected = true;
-
-                        // }
-
-                        // if ( angleFront1 - Settings.MATH_PI_025 < angleFront0 && angleFront1 + Settings.MATH_PI_025 > angleFront0 ) {
-
-                        //     vehicle1.collisionDetected = true;
-
-                        // }
-
-                        //deaktiviert, weil es aktuell sonst zu unendlich langen Rückstaus kommt die für immer mehr vehicles sorgen und die performance dann irgendwann in den Keller geht.
-                        // vehicle0.speed = this._vehicles.getVehicleSpeed( vehicle0, 0.0001 );
-                        // vehicle1.speed = this._vehicles.getVehicleSpeed( vehicle1, 0.0001 );
-                        // vehicle0.speed = 0;
-                        // vehicle1.speed = 0;
-
-                        // //funktioniert zwar, aber kostet extrem viel performance
-                        // const angle0 = Tools.getAtan2Normalized( vehicle1.position.y - vehicle0.position.y, vehicle1.position.x - vehicle0.position.x );
-                        // const angle1 = Tools.getAtan2Normalized( vehicle0.position.y - vehicle1.position.y, vehicle0.position.x - vehicle1.position.x );
-                        
-                        // //https://jsfiddle.net/NiklasKnaack/tq7a4bwg/
-                        // if ( Tools.isInsideAngle( vehicle0.angle, angle0 ) === true ) {
-
-                        //     vehicle0.collisionDetected = true;
-                        
-                        // }
-
-                        // if ( Tools.isInsideAngle( vehicle1.angle, angle1 ) === true ) {
-
-                        //     vehicle1.collisionDetected = true;
-                        
-                        // }
 
                     }
 
--- js/vehicles/Vehicles.js
+++ js/vehicles/Vehicles.js
@@ -249,7 +249,7 @@ class Vehicles {
             drawTempStuff: [],
             distanceToVehicle: 0,
             lastDistanceToVehicle: 0,
-            collisions: [],
+            // collisions: [],
 
         }
 
